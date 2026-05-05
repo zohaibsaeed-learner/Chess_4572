49fd562be179592a2ac34aec4b1fc364d480fb27
zohaibsaeed-learner <saeedzohaib677@gmail.com>
2026-05-05 11:58:04 +0500

    Initial Project Structure and Abstract Piece class.

---
+++ src/model/Piece.java
@@ -0,0 +1,15 @@
+package model;
+
+public abstract class Piece {
+    private String color;
+
+    public Piece(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol, Piece[][] board);
+}
